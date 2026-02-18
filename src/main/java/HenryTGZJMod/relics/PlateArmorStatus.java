1027d803187e1d48b6228f82668446227796416c
YiAn <2150892215@qq.com>
2026-02-19 00:33:34 +0800

    优化完善计数上限，为日后可能的用途做准备

--- src/main/java/HenryTGZJMod/relics/PlateArmorStatus.java
+++ src/main/java/HenryTGZJMod/relics/PlateArmorStatus.java
@@ -2,15 +2,10 @@ package HenryTGZJMod.relics;
 
 import HenryTGZJMod.helpers.ModHelper;
 import HenryTGZJMod.ui.campfire.RepairOption;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
-import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 import com.megacrit.cardcrawl.ui.campfire.AbstractCampfireOption;
-import com.megacrit.cardcrawl.ui.campfire.LiftOption;
 
 import java.util.ArrayList;
 
@@ -19,21 +14,23 @@ public class PlateArmorStatus extends AbstractHenryRelic {
     private static final RelicTier RELIC_TIER = RelicTier.STARTER; // 遗物类型
     private static final LandingSound LANDING_SOUND = LandingSound.FLAT; // 点击音效
 
+    public int maxCounter = 120;
+
     public PlateArmorStatus() {
         super(ID, RELIC_TIER, LANDING_SOUND);
         this.counter = 100;
     }
-    @Override
-    public void renderCounter(SpriteBatch sb, boolean inTopPanel) {
-        if (this.counter >120) {
-            this.counter = 120;
-        }
-        if (this.counter <0) {
-            this.counter = 0;
-        }
-        super.renderCounter(sb, inTopPanel);
-
-    }
+//    @Override
+//    public void renderCounter(SpriteBatch sb, boolean inTopPanel) {
+//        if (this.counter >120) {
+//            this.counter = 120;
+//        }
+//        if (this.counter <0) {
+//            this.counter = 0;
+//        }
+//        super.renderCounter(sb, inTopPanel);
+//
+//    }
 
     // 获取遗物描述，但原版游戏只在初始化和获取遗物时调用，故该方法等于初始描述
     public String getUpdatedDescription() {
--- src/main/java/HenryTGZJMod/relics/WeaponStatus.java
+++ src/main/java/HenryTGZJMod/relics/WeaponStatus.java
@@ -1,14 +1,11 @@
 package HenryTGZJMod.relics;
 
 import HenryTGZJMod.helpers.ModHelper;
-import HenryTGZJMod.ui.campfire.RepairOption;
 import HenryTGZJMod.ui.campfire.SharpenOption;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 import com.megacrit.cardcrawl.ui.campfire.AbstractCampfireOption;
 
@@ -19,21 +16,25 @@ public class WeaponStatus extends AbstractHenryRelic {
     private static final RelicTier RELIC_TIER = RelicTier.STARTER; // 遗物类型
     private static final LandingSound LANDING_SOUND = LandingSound.FLAT; // 点击音效
 
+    public int maxCounter = 120;
+
     public WeaponStatus() {
         super(ID, RELIC_TIER, LANDING_SOUND);
         this.counter = 100;
     }
-    @Override
-    public void renderCounter(SpriteBatch sb, boolean inTopPanel) {
-        if (this.counter >120) {
-            this.counter = 120;
-        }
-        if (this.counter <0) {
-            this.counter = 0;
-        }
-        super.renderCounter(sb, inTopPanel);
 
-    }
+
+//    @Override
+//    public void renderCounter(SpriteBatch sb, boolean inTopPanel) {
+//        if (this.counter >120) {
+//            this.counter = 120;
+//        }
+//        if (this.counter <0) {
+//            this.counter = 0;
+//        }
+//        super.renderCounter(sb, inTopPanel);
+//
+//    }
 
     // 获取遗物描述，但原版游戏只在初始化和获取遗物时调用，故该方法等于初始描述
     public String getUpdatedDescription() {
@@ -82,7 +83,7 @@ public class WeaponStatus extends AbstractHenryRelic {
     }
 
     public void addCampfireOption(ArrayList<AbstractCampfireOption> options) {
-        options.add(new SharpenOption(this.counter < 120));
+        options.add(new SharpenOption(this.counter < maxCounter));
     }
 
 
