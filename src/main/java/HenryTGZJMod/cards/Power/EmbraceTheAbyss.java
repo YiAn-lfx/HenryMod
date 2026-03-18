6bab4838cda0a1e79465883754bb59686e7cd9e2
YiAn <2150892215@qq.com>
2026-03-18 21:27:43 +0800

    feat: 调整“拥抱深渊”卡牌及能力

--- src/main/java/HenryTGZJMod/cards/Power/EmbraceTheAbyss.java
+++ src/main/java/HenryTGZJMod/cards/Power/EmbraceTheAbyss.java
@@ -2,35 +2,42 @@ package HenryTGZJMod.cards.Power;
 
 import HenryTGZJMod.cards.AbstractHenryCard;
 import HenryTGZJMod.helpers.ModHelper;
-import HenryTGZJMod.powers.EmbraceTheAbyssPower;
+import HenryTGZJMod.powers.BloodPactPower;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.MathUtils;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.cards.status.Burn;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.BorderLongFlashEffect;
+import com.megacrit.cardcrawl.vfx.combat.VerticalAuraEffect;
 
 
 public class EmbraceTheAbyss extends AbstractHenryCard {
     public static final String ID = ModHelper.makePath(EmbraceTheAbyss.class.getSimpleName());
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID); // 从游戏系统读取本地化资源
-    private static final int COST = 1;
-    private static final CardType TYPE = CardType.POWER; //类型
+    private static final int COST = 2;
+    private static final CardType TYPE = CardType.SKILL; //类型
     private static final CardRarity RARITY = CardRarity.RARE; //稀有度
     private static final CardTarget TARGET = CardTarget.SELF; //指向类型
 
     public EmbraceTheAbyss() {
         super(ID, false, CARD_STRINGS, COST, TYPE, RARITY, TARGET);
         this.magicNumber = this.baseMagicNumber = 5;
-        this.cardsToPreview = new Burn();
+        this.exhaust = true;
+        this.selfRetain = false;
+        //this.cardsToPreview = new Burn();
     }
 
     @Override
     public void upgrade() { //卡牌升级
         if(!this.upgraded){
             this.upgradeName();
-            this.upgradeBaseCost(0);
+            //this.upgradeBaseCost(0);
+            this.selfRetain = true;
             this.rawDescription = CARD_STRINGS.UPGRADE_DESCRIPTION; //使用升级后的本地化文本
             this.initializeDescription();
         }
@@ -40,8 +47,11 @@ public class EmbraceTheAbyss extends AbstractHenryCard {
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) { //卡牌使用效果
         CardCrawlGame.sound.playA("BELL", MathUtils.random(-0.2F, -0.3F));
-//        this.addToBot(new ApplyPowerAction(p, p, new FrailPower(p, magicNumber, false)));
-//        this.addToBot(new ApplyPowerAction(p, p, new VulnerablePower(p, magicNumber, false)));
-        this.addToBot(new ApplyPowerAction(p, p, new EmbraceTheAbyssPower(p)));
+        this.addToBot(new VFXAction(p, new VerticalAuraEffect(Color.BLACK, p.hb.cX, p.hb.cY), 0.33F));
+        this.addToBot(new SFXAction("ATTACK_FIRE"));
+        this.addToBot(new VFXAction(p, new VerticalAuraEffect(Color.PURPLE, p.hb.cX, p.hb.cY), 0.33F));
+        this.addToBot(new VFXAction(p, new VerticalAuraEffect(Color.CYAN, p.hb.cX, p.hb.cY), 0.0F));
+        this.addToBot(new VFXAction(p, new BorderLongFlashEffect(Color.MAGENTA), 0.0F, true));
+        this.addToBot(new ApplyPowerAction(p, p, new BloodPactPower(p,3)));
     }
 }
